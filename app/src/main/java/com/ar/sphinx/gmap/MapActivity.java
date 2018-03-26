bc2eed7c503cd09a596b2a80184c8dfccbf7ac5a
sPhinx <ankit.rajput@instavans.com>
2018-03-27 02:08:41 +0530

    feat: current location fetched

--- app/src/main/java/com/ar/sphinx/gmap/MapActivity.java
+++ app/src/main/java/com/ar/sphinx/gmap/MapActivity.java
@@ -3,6 +3,7 @@ package com.ar.sphinx.gmap;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,15 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Map;
 
 /**
  * Created by sPhinx on 19/03/18.
  */
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 	private static final String TAG = "MapActivity";
 	private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
@@ -28,6 +38,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 	String COARSE_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
 	private boolean locationPermissionsGranted = false;
 	private GoogleMap gMap;
+	private FusedLocationProviderClient fusedLocationProviderClient;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,24 +47,49 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 		getLocationPermissions();
 	}
 
-	public void initMap(){
-		SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-		supportMapFragment.getMapAsync(this);
+	public void getCurrentLocation() {
+		fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+		try {
+			if(locationPermissionsGranted) {
+				Task location = fusedLocationProviderClient.getLastLocation();
+				location.addOnCompleteListener(new OnCompleteListener() {
+					@Override
+					public void onComplete(@NonNull Task task) {
+						if(task.isSuccessful()) {
+							Location currentLocation = (Location) task.getResult();
+							moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 15f);
+						}
+					}
+				});
+			}
+		} catch(SecurityException ex) {
+			Log.e(TAG, "getCurrentLocation: exception.");
+		}
 	}
 
-	public void getLocationPermissions(){
+	public void moveCamera(LatLng latLng, float zoom) {
+		gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+	}
+
+	public void initMap() {
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(MapActivity.this);
+	}
+
+	public void getLocationPermissions() {
 		Log.d(TAG, "getLocationPermissions: running..");
-		String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
-		if(ContextCompat.checkSelfPermission(getApplicationContext(),FINE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
-			if(ContextCompat.checkSelfPermission(getApplicationContext(),COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+		String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+		if(ContextCompat.checkSelfPermission(getApplicationContext(), FINE_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+			if(ContextCompat.checkSelfPermission(getApplicationContext(), COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
 				locationPermissionsGranted = true;
 				Log.d(TAG, "getLocationPermissions: granted permissions :)");
-			}else {
-				ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+				initMap();
+			} else {
+				ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
 				Log.d(TAG, "getLocationPermissions: requesting permissions :)");
 			}
-		}else {
-			ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+		} else {
+			ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
 			Log.d(TAG, "getLocationPermissions: requesting permissions :)");
 		}
 	}
@@ -62,7 +98,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 //		super.onRequestPermissionsResult(requestCode,permissions,grantResults);
 		locationPermissionsGranted = false;
-		switch(requestCode){
+		switch(requestCode) {
 
 			case LOCATION_PERMISSION_REQUEST_CODE: {
 				if(grantResults.length > 0) {
@@ -83,6 +119,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
-		Toast.makeText(MapActivity.this,"Map is ready guys",Toast.LENGTH_SHORT).show();
+		Toast.makeText(MapActivity.this, "Map is ready guys", Toast.LENGTH_SHORT).show();
+		gMap = googleMap;
+		if(locationPermissionsGranted) {
+			getCurrentLocation();
+			if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+					&& ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				return;
+			}
+			gMap.setMyLocationEnabled(true);
+
+		}
 	}
 }
